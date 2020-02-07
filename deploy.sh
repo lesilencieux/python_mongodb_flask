55a98861184dc29d468fab65c3e665596b02d6f2
Romeo Adanhounme <romeoadanhounme@gmail.com>
2020-02-07 16:02:42 +0100

    add sudo to deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker stop app_h
-docker rm app_h
-docker rmi app_h_img
-docker build -t app_h_img .
-docker run -p 5001:5001 --name app_h --restart always -d app_h_img
+sudo docker stop app_h
+sudo docker rm app_h
+sudo docker rmi app_h_img
+sudo docker build -t app_h_img .
+sudo docker run -p 5001:5001 --name app_h --restart always -d app_h_img
